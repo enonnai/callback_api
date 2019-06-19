3554faf1a9f85174d23b0957908bfcf12d786a21
cristina <iannonecristina@gmail.com>
2019-06-19 01:19:24 +0100

    POST call to the API via Submit

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,24 @@
 class HomepageController < ApplicationController
+  include HTTParty
+
   def index
+  end
+
+  def create
+    response = HTTParty.post('http://mic-leads.dev-test.makeiteasy.com/api/v1/create',
+    :body => { :access_token => ENV['LEAD_API_ACCESS_TOKEN'],
+               :pGUID => '-5584-4506-9c6f-5a65e14bfd08',
+               :pAccName => '',
+               :pPartner => '',
+               :name => 'Joajdoajof',
+               :business_name => '',
+               :telephone_number => '089955857',
+               :email => 'guybrushtesterexample.com'
+             },
+    :headers => { 'Content-Type' => 'application/x-www-form-urlencoded' } )
+
+    flash.now[:errors] = ErrorFormatter.new.format(response)
+
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'homepage#index'
+  post '/', to: 'homepage#create'
 end
