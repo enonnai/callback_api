5472321d9f60502e43bf85ffcf91c09b7886886a
cristina <iannonecristina@gmail.com>
2019-06-23 17:13:37 +0100

    Handle Errors

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,5 @@
 require 'securerandom'
+require 'pry'
 
 class HomepageController < ApplicationController
   include HTTParty
@@ -7,7 +8,26 @@ class HomepageController < ApplicationController
   end
 
   def create
-    response = HTTParty.post('http://mic-leads.dev-test.makeiteasy.com/api/v1/create',
+    begin
+      response = send_callback_data
+      case response.code
+      when 500, 501, 502, 503
+        downtime_error
+      when 400, 404
+        format_errors(response)
+      when 200, 201, 202, 203
+        success_message
+      end
+    rescue => e
+      downtime_error
+    end
+    render :index
+  end
+
+  private
+
+  def send_callback_data
+    HTTParty.post('http://mic-leads.dev-test.makeiteasy.com/api/v1/create',
     :body => { :access_token =>     ENV['LEAD_API_ACCESS_TOKEN'],
                :pGUID =>            SecureRandom.uuid,
                :pAccName =>         'Account',
@@ -16,11 +36,18 @@ class HomepageController < ApplicationController
                :business_name =>    params["contact_form"][:business_name],
                :telephone_number => params["contact_form"][:telephone_number],
                :email =>            params["contact_form"][:email]
-             },
-    :headers => { 'Content-Type' => 'application/x-www-form-urlencoded' } )
+             },:headers => { 'Content-Type' => 'application/x-www-form-urlencoded' } )
+  end
 
+  def downtime_error
+    flash.now[:errors] = ['Sorry, our services are temporarily down. Please try again later. If the problem persists, please contact support.']
+  end
+
+  def format_errors(response)
     flash.now[:errors] = ErrorFormatter.new.format(response)
+  end
 
-    render :index
+  def success_message
+    flash.now[:message] = "Thank you! Your request was submitted successfully! Makeitcheaper will contact you soon."
   end
 end
--- spec/controllers/homepage_controller_spec.rb
+++ spec/controllers/homepage_controller_spec.rb
@@ -9,38 +9,78 @@ RSpec.describe HomepageController do
   end
 
   describe "POST create" do
-    before(:each) do
-      allow(SecureRandom).to receive(:uuid).and_return("189a607d-9950-4f8c-845e-20d4b0b4b3f4")
 
-      stub_request(:post, "http://mic-leads.dev-test.makeiteasy.com/api/v1/create").
-         with(
-           body: {"access_token"=>"fb1988cfdd74e7ecb2ea291e2096b44e",
-                   "business_name"=>    "Hello Fresh Ltd",
-                   "email"=>            "hello@example.com",
-                   "name"=>             "Guybrush Threepwood",
-                   "pAccName"=>         "Account",
-                   "pGUID"=>            "189a607d-9950-4f8c-845e-20d4b0b4b3f4",
-                   "pPartner"=>         "Partner",
-                   "telephone_number"=> "07456888596"},
-             headers: {
-               'Content-Type'=>'application/x-www-form-urlencoded'
-            }
-          ).
-      to_return(status: 200, body: '{"message":"Enqueue success","errors":[]}', headers: {'Content-Type' => 'application/json'},)
-      post :create, params: {"contact_form"=>{"first_name_last_name"=>"Guybrush Threepwood", "business_name"=>"Hello Fresh Ltd", "telephone_number"=>"07456888596", "email"=>"hello@example.com"} }
+    context "when the API DOES NOT raise an exception after receiving a POST request" do
+      before(:each) do
+        allow(SecureRandom).to receive(:uuid).and_return("189a607d-9950-4f8c-845e-20d4b0b4b3f4")
+        stub_request(:post, "http://mic-leads.dev-test.makeiteasy.com/api/v1/create").
+           with(
+             body: {"access_token"=>"fb1988cfdd74e7ecb2ea291e2096b44e",
+                     "business_name"=>    "Hello Fresh Ltd",
+                     "email"=>            "hello@example.com",
+                     "name"=>             "Guybrush Threepwood",
+                     "pAccName"=>         "Account",
+                     "pGUID"=>            "189a607d-9950-4f8c-845e-20d4b0b4b3f4",
+                     "pPartner"=>         "Partner",
+                     "telephone_number"=> "07456888596"},
+               headers: {
+                 'Content-Type'=>'application/x-www-form-urlencoded'
+              }
+            ).
+        to_return(status: 200, body: '{"message":"Enqueue success","errors":[]}', headers: {'Content-Type' => 'application/json'})
+
+        post :create, params: {"contact_form"=>{"first_name_last_name"=>"Guybrush Threepwood", "business_name"=>"Hello Fresh Ltd", "telephone_number"=>"07456888596", "email"=>"hello@example.com"} }
+      end
+
+      it "renders the index page" do
+        expect(response).to render_template("index")
+      end
+
+      it "makes a POST request to the Makeiteasy API" do
+        expect(WebMock).to have_requested(:post, 'http://mic-leads.dev-test.makeiteasy.com/api/v1/create').
+        with(body: 'access_token=fb1988cfdd74e7ecb2ea291e2096b44e&pGUID=189a607d-9950-4f8c-845e-20d4b0b4b3f4&pAccName=Account&pPartner=Partner&name=Guybrush%20Threepwood&business_name=Hello%20Fresh%20Ltd&telephone_number=07456888596&email=hello%40example.com',
+          headers: {
+           'Content-Type'=>'application/x-www-form-urlencoded'
+          }
+        ).once
+      end
     end
 
-    it "renders the index page" do
-      expect(response).to render_template("index")
+    context "when the API raises an exception after receiving a POST request" do
+      before(:each) do
+        allow(HTTParty).to receive(:post).and_raise(HTTParty::Error)
+
+        post :create, params: {"contact_form"=>{"first_name_last_name"=>"Guybrush Threepwood", "business_name"=>"Hello Fresh Ltd", "telephone_number"=>"07456888596", "email"=>"hello@example.com"} }
+      end
+
+      it "renders the index page" do
+        expect(response).to render_template("index")
+      end
     end
 
-    it "makes a POST request to the Makeiteasy API" do
-      expect(WebMock).to have_requested(:post, 'http://mic-leads.dev-test.makeiteasy.com/api/v1/create').
-      with(body: 'access_token=fb1988cfdd74e7ecb2ea291e2096b44e&pGUID=189a607d-9950-4f8c-845e-20d4b0b4b3f4&pAccName=Account&pPartner=Partner&name=Guybrush%20Threepwood&business_name=Hello%20Fresh%20Ltd&telephone_number=07456888596&email=hello%40example.com',
-        headers: {
-         'Content-Type'=>'application/x-www-form-urlencoded'
-        }
-      ).once
+    context "when the API returns an Internal Server Error POST request" do
+      before(:each) do
+        allow(SecureRandom).to receive(:uuid).and_return("189a607d-9950-4f8c-845e-20d4b0b4b3f4")
+        stub_request(:post, "http://mic-leads.dev-test.makeiteasy.com/api/v1/create").
+           with(
+             body: {"access_token"=>"fb1988cfdd74e7ecb2ea291e2096b44e",
+                     "business_name"=>    "Hello Fresh Ltd",
+                     "email"=>            "hello@example.com",
+                     "name"=>             "Guybrush Threepwood",
+                     "pAccName"=>         "Account",
+                     "pGUID"=>            "189a607d-9950-4f8c-845e-20d4b0b4b3f4",
+                     "pPartner"=>         "Partner",
+                     "telephone_number"=> "07456888596"},
+               headers: {
+                 'Content-Type'=>'application/x-www-form-urlencoded'
+              }).to_return(status: [500, "Internal Server Error"])
+
+        post :create, params: {"contact_form"=>{"first_name_last_name"=>"Guybrush Threepwood", "business_name"=>"Hello Fresh Ltd", "telephone_number"=>"07456888596", "email"=>"hello@example.com"} }
+      end
+
+      it "renders the index page" do
+        expect(response).to render_template("index")
+      end
     end
   end
 end
